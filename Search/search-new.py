7ba91e8cbfaffe2c792eb64838081ddd41501122
aneeshpatne <aneeshpatne@gmail.com>
2025-02-20 11:48:37 +0530

    refactor: rename SearchAgent to deepSearch and add searchAgent for result retrieval

--- Search/search-new.py
+++ Search/search-new.py
@@ -1,6 +1,5 @@
 from duckduckgo_search import DDGS
 from langchain_openai import ChatOpenAI
-from browser_use import Controller, Agent
 from pydantic import SecretStr
 from dotenv import load_dotenv
 import os
@@ -9,14 +8,21 @@ load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 ddgs = DDGS()
 llm=ChatOpenAI(model='gpt-4o-mini-2024-07-18', api_key=SecretStr(api_key), temperature=0.0)
-async def SearchAgent(topic):
+def deepSearch(topic):
     search_prompt = (
         f"The user prompt is: {topic}. "
         "you are a step before the data is fed to the API. you need to take this user prompt and convert this into a search query. "
+        "to deep search, generate 5 more search queries based on the user prompt. "
         "return the search query, only and nothing else."
     )
     response = llm.invoke(search_prompt)
     print("Search Query:", response.content)
+
+def searchAgent(topic):
+    result = ddgs.text(topic, max_results=5)
+    link = [r['href'] for r in result]
+    print("Search Results:", link)
+
 if __name__ == "__main__":
-    topic = "maharashtra latest news"
-    asyncio.run(SearchAgent(topic))
+    topic = "Elon Musk"
+    searchAgent(topic)
