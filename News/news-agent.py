0163bf1fc498776770fa0296aab6f9229090986b
aneeshpatne <aneeshpatne@gmail.com>
2025-03-02 12:24:32 +0530

    feat: update news agent with new LLM configurations for summary and deep search

--- News/news-agent.py
+++ News/news-agent.py
@@ -11,3 +11,10 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain.output_parsers import PydanticOutputParser
 from langchain.output_parsers.retry import RetryWithErrorOutputParser
 from langchain.memory import ConversationBufferMemory
+
+
+#summary_llm = ChatOpenAI(base_url='https://openrouter.ai/api/v1', model='meta-llama/llama-3.3-70b-instruct:nitro', api_key=SecretStr(openRouterKey))
+agent_llm = ChatOpenAI(model='gpt-4o-mini', api_key=SecretStr(api_key))
+#deep_search_llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-thinking-exp-01-21', api_key=SecretStr(geminiKey))
+summary_llm = ChatOpenAI(model='gpt-4o-mini', api_key=SecretStr(api_key))
+deep_search_llm = ChatOpenAI(model='o1-mini', api_key=SecretStr(api_key))
