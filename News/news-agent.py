d2c3b20598c74c4ce79c016bd5d3bbb9101dca8c
aneeshpatne <aneeshpatne@gmail.com>
2025-03-02 12:25:29 +0530

    feat: add environment variable support and user agent options in news agent

--- News/news-agent.py
+++ News/news-agent.py
@@ -12,9 +12,23 @@ from langchain.output_parsers import PydanticOutputParser
 from langchain.output_parsers.retry import RetryWithErrorOutputParser
 from langchain.memory import ConversationBufferMemory
 
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+openRouterKey = os.getenv("OPEN_ROUTER_KEY")
+geminiKey = os.getenv("GEMINI_API_KEY")
 
+# ---------------------------------
+# LLM Options
 #summary_llm = ChatOpenAI(base_url='https://openrouter.ai/api/v1', model='meta-llama/llama-3.3-70b-instruct:nitro', api_key=SecretStr(openRouterKey))
 agent_llm = ChatOpenAI(model='gpt-4o-mini', api_key=SecretStr(api_key))
 #deep_search_llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash-thinking-exp-01-21', api_key=SecretStr(geminiKey))
 summary_llm = ChatOpenAI(model='gpt-4o-mini', api_key=SecretStr(api_key))
 deep_search_llm = ChatOpenAI(model='o1-mini', api_key=SecretStr(api_key))
+
+# ---------------------------------
+# User Agent options to avoid blocking.
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5414.120 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.198 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+]
